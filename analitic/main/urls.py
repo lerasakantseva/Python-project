6491801dd9da37acaa8bdfc00fb4487a6be16798
Lera <LeraSakantseva3@yandex.ru>
2024-01-15 22:54:11 +0500

    make django project

--- analitic/main/urls.py
+++ analitic/main/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index),
-    path('relevance', views.relevance),
-    path('geography', views.geography),
-    path('skills', views.skills),
-    path('last_vacancies', views.last_vacancies)
+    path('', views.index, name="main"),
+    path('relevance', views.relevance, name='relevance'),
+    path('geography', views.geography, name='geography'),
+    path('skills', views.skills, name='skills'),
+    path('last_vacancies', views.last_vacancies, name='last_vacancies')
 ]
