0e195c24dc6196f40ea06bc2fafbc37ff3461ef1
Lera <LeraSakantseva3@yandex.ru>
2024-01-22 16:47:30 +0500

    проект конечный

--- analitic/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.index, name="main"),
-    path('relevance', views.relevance, name='relevance'),
-    path('geography', views.geography, name='geography'),
-    path('skills', views.skills, name='skills'),
-    path('last_vacancies', views.last_vacancies, name='last_vacancies')
-]
